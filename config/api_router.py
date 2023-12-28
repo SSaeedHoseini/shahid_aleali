e1ad71a66931c242f87234672f01d3c3668c176a
ho <hoseini.py@gmail.com>
2023-12-28 10:25:50 +0330

    feture: add schools apps

--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,23 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from shahid_aleali.users.api.views import UserViewSet, GroupViewSet
+from schools.views import (
+    AcademicYearModelViewSet,
+    AttendanceModelViewSet,
+    ClassModelViewSet,
+    ClassScheduleModelViewSet,
+    ExamModelViewSet,
+    ExamScoreModelViewSet,
+    GradeLevelModelViewSet,
+    LessonModelViewSet,
+    ParentModelViewSet,
+    SchoolModelViewSet,
+    StudentModelViewSet,
+    SubjectModelViewSet,
+    TeacherModelViewSet,
+    TeacherSubjectModelViewSet,
+)
+from shahid_aleali.users.api.views import GroupViewSet, UserViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -10,6 +26,20 @@ else:
 
 router.register("users", UserViewSet)
 router.register("groups", GroupViewSet)
+router.register("academic-year", AcademicYearModelViewSet)
+router.register("attendances", AttendanceModelViewSet)
+router.register("classes", ClassModelViewSet)
+router.register("class-schedule", ClassScheduleModelViewSet)
+router.register("exams", ExamModelViewSet)
+router.register("exam-scores", ExamScoreModelViewSet)
+router.register("grade-levels", GradeLevelModelViewSet)
+router.register("lessons", LessonModelViewSet)
+router.register("parents", ParentModelViewSet)
+router.register("schools", SchoolModelViewSet)
+router.register("students", StudentModelViewSet)
+router.register("subjects", SubjectModelViewSet)
+router.register("teachers", TeacherModelViewSet)
+router.register("teacher-subjects", TeacherSubjectModelViewSet)
 
 
 app_name = "api"
