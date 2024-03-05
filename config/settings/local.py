31c0e466473dea43473554a8de83330ee0049eed
ho <hoseini.py@gmail.com>
2024-03-05 13:33:48 +0330

    change domain shahid-aleali

--- config/settings/local.py
+++ config/settings/local.py
@@ -64,3 +64,5 @@ INSTALLED_APPS += ["django_extensions"]  # noqa: F405
 CELERY_TASK_EAGER_PROPAGATES = True
 # Your stuff...
 # ------------------------------------------------------------------------------
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
